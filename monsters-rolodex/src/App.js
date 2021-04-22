f44205e9d938136f42876dbac29caa3bff64dc17
Chris Werkheiser <cjwerk@gmail.com>
2021-04-21 19:58:16 -0700

    add card list

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -1,31 +1,30 @@
-import logo from "./logo.svg";
+// import logo from "./logo.svg";
 import "./App.css";
 import React, { Component } from "react";
+import {CardList} from './components/card-list/card-list.component'
 class App extends Component {
   constructor(){
     super();
   
   this.state = {
-    string: "Heyyyyyy"
+    monsters: []
   }
+}
+
+componentDidMount() {
+  fetch('https://jsonplaceholder.typicode.com/users')
+  .then(response => response.json())
+  .then(users => this.setState({ monsters: users }));
 }
   render() {
    
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>{this.state.string} </p>
-          <button onClick={() => this.setState({ string: "I was touched!" })}>Change ME!</button>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <CardList name="Chris">
+      {
+        this.state.monsters.map(monster => <h1 key={monster.id}> { monster.name } </h1>)
+      }
+</CardList>
       </div>
     );
   }
